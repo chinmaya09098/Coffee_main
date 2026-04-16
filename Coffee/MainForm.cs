545210c51be6ed765e568b2ba83179984d943343
chinmaya09098 <chinmaya.g@quadranttechnology.com>
2026-04-16 08:47:24 -0700

    Fix: System.FormatException: Index (zero based) must be greater t

--- Coffee/MainForm.cs
+++ Coffee/MainForm.cs
@@ -265,7 +265,7 @@ namespace Coffee
         {
             if (currentCup == null) return;
             // Bug: format string has {2} but only 2 arguments provided
-            lstOrders.Items.Add(string.Format("Order {0}: {1} [ref:{2}]", lstOrders.Items.Count + 1, currentCup.Details()));
+            lstOrders.Items.Add(string.Format("Order {0}: {1}", lstOrders.Items.Count + 1, currentCup.Details()));
             ResetForm();
             ShowStatus("Order placed!");
         }
